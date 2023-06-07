70167848d6505f583f162bf187b2955f16d741de
sh.park <sh.park@petdoc.co.kr>
2023-06-07 17:24:37 +0900

    Chore: tailwind.config > main color code 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      height: {
-        header: '48px',
-      },
-      colors: {
-        background: '#fafafa',
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			height: {
+				header: '48px',
+			},
+			colors: {
+				main: '#84B1ED',
+				background: '#fafafa',
+			},
+		},
+	},
+	plugins: [],
 };
