ebc61da0a95d5b74572a82242c5845df4bc18d1b
sh.park <se9oo@kakao.com>
2023-06-27 14:04:17 +0900

    Style: 로그인 페이지 padding top 제거

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,7 +4,7 @@ import { getProviders } from 'next-auth/react';
 const Login = async () => {
 	const providers = await getProviders();
 	return (
-		<main className="relative flex flex-col justify-center w-full h-full px-6 pt-12">
+		<main className="relative flex flex-col justify-center w-full h-full px-6">
 			<SignInProviderList providers={providers} />
 		</main>
 	);
