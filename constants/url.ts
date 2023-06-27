cf1eeb4ca41caf2840c8707b0792e392da23554d
sh.park <se9oo@kakao.com>
2023-06-27 13:58:42 +0900

    Chore: 로그인 버튼 소셜 이미지 링크 추가

--- constants/url.ts
+++ constants/url.ts
@@ -3,3 +3,9 @@ export const INTERNAL_URLS = {
 	HOME: '/home',
 	LOGIN: '/login',
 };
+
+export const EXTERNAL_URLS = {
+	GITHUB_LOGO: 'https://github.githubassets.com/favicons/favicon-dark.png',
+	KAKAO_LOGO: 'https://www.kakaocorp.com/page/favicon.ico',
+	NAVER_LOGO: 'https://logoproject.naver.com/favicon.ico',
+};
